a0dc1e922f08c0e6cc7bb883c8a75fe232711920
NishizawaShota <nishizawa@emol.jp>
2020-04-18 10:55:37 +0900

    Article, Userエンティティを作成

---
+++ SwiftUIQiitaClientMVVMTest/Repositories/QiitaAPI/QiitaResponses.swift
@@ -0,0 +1,23 @@
+//
+//  Response.swift
+//  SwiftUIQiitaClientMVVMTest
+//
+//  Created by Shota Nishizawa on 2020/04/18.
+//  Copyright © 2020 Shota Nishizawa. All rights reserved.
+//
+
+import Foundation
+
+struct Article: Codable {
+    let id: String
+    let title: String
+    let user: User
+    let body: String
+    let likes_count: Int
+}
+
+struct User: Codable {
+    let name: String
+    let followees_count: Int
+    let followers_count: Int
+}
